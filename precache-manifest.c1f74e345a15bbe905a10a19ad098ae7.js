994c8761c48e6342f8ca20b5ffdf0b18508c1e39
MohitDhungana <dhunganamohit@gmail.com>
2020-07-16 16:28:31 +0545

    Updates

---
+++ precache-manifest.c1f74e345a15bbe905a10a19ad098ae7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a86edc894704016ef3a85c0b8fd93c2",
+    "url": "/ncdc-frontend/index.html"
+  },
+  {
+    "revision": "ba935554f79b1d851d19",
+    "url": "/ncdc-frontend/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "01131ead419cbcdc7d5d",
+    "url": "/ncdc-frontend/static/css/main.fccd8b03.chunk.css"
+  },
+  {
+    "revision": "ba935554f79b1d851d19",
+    "url": "/ncdc-frontend/static/js/2.382191ba.chunk.js"
+  },
+  {
+    "revision": "109083572dc3ba74f105a9f6f5c9e305",
+    "url": "/ncdc-frontend/static/js/2.382191ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01131ead419cbcdc7d5d",
+    "url": "/ncdc-frontend/static/js/main.c4e6b288.chunk.js"
+  },
+  {
+    "revision": "7b7e72932d5f3d388353",
+    "url": "/ncdc-frontend/static/js/runtime-main.e336bbed.js"
+  },
+  {
+    "revision": "6158a48ae59be9745fa3c7846f0fb4b8",
+    "url": "/ncdc-frontend/static/media/ncdc.6158a48a.jpg"
+  }
+]);
