c618303664401b02f3d4d088b5d3abc08fe4cb19
MohitDhungana <dhunganamohit@gmail.com>
2020-07-19 14:01:56 +0545

    about us page prep

---
+++ src/components/BoardMembers.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './boardmembers.css';
+
+import mani from '../images/mani.jpg';
+
+const BoardMembers = () => {
+  return (
+    <div className="row section">
+      <blockquote className="red-text text-darken-2">
+        <h3>Board Members</h3>
+      </blockquote>
+
+      <div class="row">
+        <div class="col s6 m4 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src={mani} />
+            </div>
+            <div class="card-content center-align">
+              <p>
+                Mani Dhungana <br />
+                President
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoardMembers;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import AboutUsContent from '../components/AboutUsContent';
+import BoardMembers from '../components/BoardMembers';
 
 const AboutUs = () => {
-  return <div>about us page</div>;
+  return (
+    <div className="container">
+      <AboutUsContent />
+      <BoardMembers />
+    </div>
+  );
 };
 
 export default AboutUs;
